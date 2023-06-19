f23682a06288e4825a311d7ae80046618657a02b
Elman Islam <eislam@uoguelph.ca>
2023-06-19 16:00:52 -0400

    Add update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-$("intro").children().fadeIn();
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 500) {
-    $('#about-me').children().fadeIn();
-  } else {
-    $('#about-me').children().fadeOut();
-  }
-});
+
+var bruh = 0;
+function changeBackgroundSonova() {
+  let experiences = document.getElementById("experiences");
+  experiences.style.backgroundImage = "url(images/Sonova_Hearing_Facility.jpg)";
+}
+
+
+function changeBackgroundCooperators() {
+  let experiences = document.getElementById("experiences");
+  experiences.style.backgroundImage = "url(images/Cooperators.jpg)";
+}
+
+function openResume() {
+  window.open('resume.pdf');
+}
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
